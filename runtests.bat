c1e3442502893e146efcb0678dc8e5c2c501ccff
d-markey <1548408+d-markey@users.noreply.github.com>
2022-03-20 18:51:24 +0100

    - rename "tools" to "tool"

--- runtests.bat
+++ runtests.bat
@@ -4,4 +4,4 @@ mkdir .\test\coverage
 call .\test\compile_workers.bat
 call dart run test --coverage=.\test\coverage
 call dart pub global run coverage:format_coverage --packages=.packages --report-on=lib --lcov -o .\test\coverage\lcov.info -i .\test\coverage
-java -jar .\tools\jgenhtml\jgenhtml-1.5.jar .\test\coverage\lcov.info --output-directory .\coverage  
+java -jar .\tool\jgenhtml\jgenhtml-1.5.jar .\test\coverage\lcov.info --output-directory .\coverage  
