866e4e9856501bcebd8bf6376afed10e26f029f1
d-markey <1548408+d-markey@users.noreply.github.com>
2022-04-03 18:05:11 +0200

    automate worker compilation + get status

--- test/compile_workers.sh
+++ test/compile_workers.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-declare -i compile_status = 0
+declare -i compile_status=0
 
-workers_minified = ("bitcoin" "cache" "echo" "failing" "invalid" "local_client" "pi_digits" "prime" "sample")
+workers_minified=("bitcoin" "cache" "echo" "failing" "invalid" "local_client" "pi_digits" "prime" "sample")
 for w in ${workers_minified[@]}
 do
     dart compile js -m -o "./test/sample_js_workers/${w}_worker.dart.js" "./test/sample_js_workers/${w}_worker.dart"
     if [ "$compile_status" -eq 0 ]
     then
-        compile_status = $?
+        compile_status=$?
     fi
 done
 
-workers_unminified = ("rogue")
+workers_unminified=("rogue")
 for w in ${workers_unminified[@]}
 do
     dart compile js -o "./test/sample_js_workers/${w}_worker.dart.js" "./test/sample_js_workers/${w}_worker.dart"
     if [ "$compile_status" -eq 0 ]
     then
-        compile_status = $?
+        compile_status=$?
     fi
 done
 
