e6accd85c74d306d26b3b1b03638df06738e21a2
d-markey <1548408+d-markey@users.noreply.github.com>
2022-04-03 18:09:13 +0200

    testing failure of Worker compilation

--- test/compile_workers.sh
+++ test/compile_workers.sh
@@ -4,16 +4,20 @@ declare -i compile_status=0
 workers_minified=("bitcoin" "cache" "echo" "failing" "invalid" "local_client" "pi_digits" "prime" "sample")
 for w in ${workers_minified[@]}
 do
+    echo compile_status=$compile_status
     dart compile js -m -o "./test/sample_js_workers/${w}_worker.dart.js" "./test/sample_js_workers/${w}_worker.dart"
     if [ "$compile_status" -eq 0 ]
     then
         compile_status=$?
     fi
+    echo compile_status=$compile_status
+    compile_status=1
 done
 
 workers_unminified=("rogue")
 for w in ${workers_unminified[@]}
 do
+    echo compile_status=$compile_status
     dart compile js -o "./test/sample_js_workers/${w}_worker.dart.js" "./test/sample_js_workers/${w}_worker.dart"
     if [ "$compile_status" -eq 0 ]
     then
@@ -21,4 +25,5 @@ do
     fi
 done
 
+echo compile_status=$compile_status
 exit $compile_status
