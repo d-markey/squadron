1cc7d6262622be3f39424d35002d7a4064c9ff3d
d-markey <1548408+d-markey@users.noreply.github.com>
2022-04-03 17:23:15 +0200

    Return test status from runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,9 @@
 rm -rf ./test/coverage
 mkdir ./test/coverage
 dart run test --coverage=./test/coverage
+test_status=$?
 ls -R ./test/coverage
 dart pub global run coverage:format_coverage --packages=.packages --report-on=lib --lcov -o ./test/coverage/lcov.info -i ./test/coverage
 rm -rf ./coverage /s /q
 java -jar ./tool/jgenhtml/jgenhtml-1.5.jar ./test/coverage/lcov.info --output-directory ./coverage  
+exit $test_status
