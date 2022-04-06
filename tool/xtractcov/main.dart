b3381713aff8270244938b3e3974d3d9a9f0b99a
d-markey <1548408+d-markey@users.noreply.github.com>
2022-04-06 23:59:46 +0200

    Commit coverage metrics + tool

---
+++ tool/xtractcov/main.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  try {
+    final input = await File('./coverage/xml/index.xml').readAsString();
+    final linesValid = int.tryParse(
+        RegExp('\\<coverage\\s+.*lines-valid="(\\d+)"')
+                .firstMatch(input)
+                ?.group(1) ??
+            '');
+    final lineRate = double.tryParse(
+        RegExp('\\<coverage\\s+.*line-rate="(\\d+\\.\\d+)"')
+                .firstMatch(input)
+                ?.group(1) ??
+            '');
+    if (linesValid == null) {
+      throw Exception('lines-valid not found');
+    }
+    if (lineRate == null) {
+      throw Exception('lines-valid not found');
+    }
+    await File('./coverage.json').writeAsString(jsonEncode({
+      'linesValid': linesValid,
+      'lineRate': (lineRate * 1000).toInt() / 10
+    }));
+    exit(0);
+  } catch (ex) {
+    stderr.writeln(ex);
+    exit(1);
+  }
+}
