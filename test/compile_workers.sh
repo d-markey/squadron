acb5dded4b574f1d4dee3277d2b30e3677e8fac2
d-markey <1548408+d-markey@users.noreply.github.com>
2022-05-01 02:03:49 +0200

    - test workers build shell script

--- test/compile_workers.sh
+++ test/compile_workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -i compile_status=0
 
-workers_minified=("cache_worker" "echo_worker" "invalid_worker" "issues_worker" "local_client_worker" "prime_worker")
+workers_minified=("cache_worker" "echo_worker" "issues_worker" "local_client_worker" "prime_worker")
 for w in ${workers_minified[@]}
 do
     dart compile js -m -o "./test/sample_js_workers/${w}.dart.js" "./test/sample_js_workers/${w}.dart"
@@ -11,7 +11,7 @@ do
     fi
 done
 
-workers_unminified=("test_worker", "test_worker_failing", "test_worker_invalid")
+workers_unminified=("test_worker" "test_worker_failing" "test_worker_invalid")
 for w in ${workers_unminified[@]}
 do
     dart compile js -o "./test/sample_js_workers/${w}.dart.js" "./test/sample_js_workers/${w}.dart"
