34835cc2ecd052b86cd13bee1f30afea0f01bd03
d-markey <d-markey@users.noreply.github.com>
2023-09-15 23:45:39 +0200

    Update web worker compilation script for Linux

--- test/compile_workers.sh
+++ test/compile_workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -i compile_status=0
 
-workers_minified=("cache_worker" "echo_worker" "issues_worker" "local_client_worker" "prime_worker")
+workers_minified=("cache_worker" "echo_worker" "installable_worker" "issues_worker" "local_client_worker" "prime_worker")
 for w in ${workers_minified[@]}
 do
     dart compile js -m -o "./test/sample_js_workers/${w}.dart.js" "./test/sample_js_workers/${w}.dart"
