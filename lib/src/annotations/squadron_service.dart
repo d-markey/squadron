3b08191415c98032ece5c5157974cf9ed8dd37f1
d-markey <d-markey@users.noreply.github.com>
2024-08-26 19:06:42 +0200

    Add option to select Web Assembly vs. JavaScript workers in SquadronService annotation.

--- lib/src/annotations/squadron_service.dart
+++ lib/src/annotations/squadron_service.dart
@@ -2,14 +2,20 @@ import '../pool/worker_pool.dart';
 
 /// Annotation for service classes to be wrapped as workers.
 class SquadronService {
-  const SquadronService(
-      {this.pool = true, this.vm = true, this.web = true, this.baseUrl = ''});
+  const SquadronService({
+    this.pool = true,
+    this.vm = true,
+    this.web = true,
+    this.wasm = false,
+    this.baseUrl = '',
+  });
 
-  const SquadronService.web({bool pool = true, required String baseUrl})
-      : this(pool: pool, vm: false, web: true, baseUrl: baseUrl);
+  const SquadronService.web(
+      {bool pool = true, bool wasm = false, required String baseUrl})
+      : this(pool: pool, vm: false, web: true, wasm: wasm, baseUrl: baseUrl);
 
   const SquadronService.vm({bool pool = true})
-      : this(pool: pool, vm: true, web: false);
+      : this(pool: pool, vm: true, web: false, wasm: false);
 
   /// Controls code generation of a [WorkerPool] exposing the target service class.
   /// `true` by default.
@@ -22,6 +28,10 @@ class SquadronService {
   /// Controls code generation of a Web entry point (for Web Worker). `true` by
   /// default.
   final bool web;
+
+  /// Controls entry-point extension for Web worker URIs: '.js' if `false`, '.wasm'
+  /// if `true`. By default, JavaScript Web workers are used.
+  final bool wasm;
 
   /// For Web-based workers, indicates the [baseUrl] where the Web Worker will
   /// be exposed in production.
