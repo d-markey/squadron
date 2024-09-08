bb7b36436e7a33ab9f1b602ecf8a8a6199699fc6
d-markey <d-markey@users.noreply.github.com>
2024-09-08 15:35:40 +0200

    workflows

--- tool/compile_test_workers.sh
+++ tool/compile_test_workers.sh
@@ -23,12 +23,12 @@ compile "js"   "-O4" "./test/workers/js/local_client_worker.dart"  "./test/worke
 compile "js"   "-O4" "./test/workers/js/prime_worker.dart"         "./test/workers/js/prime_worker.dart.js"
 compile "js"   ""    "./test/workers/js/test_worker.dart"          "./test/workers/js/test_worker.dart.js"
 
-compile "wasm" ""    "./test/workers/js/cache_worker.dart"         "./test/workers/wasm/cache_worker.dart.wasm"
-compile "wasm" ""    "./test/workers/wasm/echo_worker.dart"        "./test/workers/wasm/echo_worker.dart.wasm"
-compile "wasm" ""    "./test/workers/js/installable_worker.dart"   "./test/workers/wasm/installable_worker.dart.wasm"
-compile "wasm" ""    "./test/workers/js/issues_worker.dart"        "./test/workers/wasm/issues_worker.dart.wasm"
-compile "wasm" ""    "./test/workers/js/local_client_worker.dart"  "./test/workers/wasm/local_client_worker.dart.wasm"
-compile "wasm" ""    "./test/workers/js/prime_worker.dart"         "./test/workers/wasm/prime_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/js/cache_worker.dart"         "./test/workers/wasm/cache_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/wasm/echo_worker.dart"        "./test/workers/wasm/echo_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/js/installable_worker.dart"   "./test/workers/wasm/installable_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/js/issues_worker.dart"        "./test/workers/wasm/issues_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/js/local_client_worker.dart"  "./test/workers/wasm/local_client_worker.dart.wasm"
+compile "wasm" "-O4" "./test/workers/js/prime_worker.dart"         "./test/workers/wasm/prime_worker.dart.wasm"
 compile "wasm" ""    "./test/workers/js/test_worker.dart"          "./test/workers/wasm/test_worker.dart.wasm"
 
 popd
--- tool/pub_test_console.sh
+++ tool/pub_test_console.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd "$(dirname $0)/.."
+pushd $(dirname $0)/..
 
 publish() { # arguments: folder
     cp ./test/$1/*.html ./doc/$1
@@ -12,19 +12,23 @@ publish() { # arguments: folder
     cp ./test/$1/*.deps ./doc/$1
 }
 
-dart compile js -o ./test/browser-tests/index.dart.js ./test/browser-tests/index.dart
-dart compile js -o ./test/browser-tests/test_runner_js2js.dart.js ./test/browser-tests/test_runner_js2js.dart
-dart compile js -o ./test/browser-tests/test_runner_js2wasm.dart.js ./test/browser-tests/test_runner_js2wasm.dart
-dart compile wasm -o ./test/browser-tests/test_runner_wasm2js.dart.wasm ./test/browser-tests/test_runner_wasm2js.dart
-dart compile wasm -o ./test/browser-tests/test_runner_wasm2wasm.dart.wasm ./test/browser-tests/test_runner_wasm2wasm.dart
+dart compile js   -O4 -o ./test/browser-tests/index.dart.js                   ./test/browser-tests/index.dart
+dart compile js   -O4 -o ./test/browser-tests/test_runner_js2js.dart.js       ./test/browser-tests/test_runner_js2js.dart
+dart compile js   -O4 -o ./test/browser-tests/test_runner_js2wasm.dart.js     ./test/browser-tests/test_runner_js2wasm.dart
+dart compile wasm -O4 -o ./test/browser-tests/test_runner_wasm2js.dart.wasm   ./test/browser-tests/test_runner_wasm2js.dart
+dart compile wasm -O4 -o ./test/browser-tests/test_runner_wasm2wasm.dart.wasm ./test/browser-tests/test_runner_wasm2wasm.dart
 
 publish browser_tests
 publish workers/js
 publish workers/wasm
 
+git stash
+git checkout gh-pages
+git stash apply
+
 git config --global user.name 'd-markey'
 git config --global user.email 'd-markey@users.noreply.github.com'
-git commit -am "Test console"
+git commit -am Test console
 git push
 
 popd
